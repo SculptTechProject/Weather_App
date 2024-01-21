026dc8bb2315083d459ce72730ae7bdf716329e0
SculptTechProject <sculpttechproject@gmail.com>
2024-01-22 00:51:04 +0100

    Changing directory of scripts, adding Flask, creating directory for images.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask("Website")
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+
+app.run(debug=True)
